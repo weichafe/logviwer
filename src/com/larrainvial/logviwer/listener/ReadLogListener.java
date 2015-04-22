04ff022e4e06e6ebb0808660d79b9ffba4d76cfe
unknown <vnazar@vnazar.larra_dom>
2015-04-22 15:13:47 -0300

    sss

--- src/com/larrainvial/logviwer/listener/ReadLogListener.java
+++ src/com/larrainvial/logviwer/listener/ReadLogListener.java
@@ -5,11 +5,17 @@ import com.larrainvial.logviwer.event.StringToFixMessageEvent;
 import com.larrainvial.trading.emp.Controller;
 import com.larrainvial.trading.emp.Event;
 import com.larrainvial.trading.emp.Listener;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class ReadLogListener implements Listener {
+
+    private long
 
     @Override
     public void eventOccurred(Event event){
@@ -29,13 +35,27 @@ public class ReadLogListener implements Listener {
 
         try {
 
-            String lineFromLog;
-            FileReader f = new FileReader(namefile);
+
+            File file = new File(namefile);
+            FileReader f = new FileReader(file);
             BufferedReader b = new BufferedReader(f);
 
-            while ((lineFromLog = b.readLine()) != null){
-                Controller.dispatchEvent(new StringToFixMessageEvent(this, lineFromLog, nameAlgo, typeMarket));
-            }
+            TimerTask timerTask = new TimerTask(){
+
+                public void run() {
+                    String lineFromLog;
+                    while ((lineFromLog = b.readLine()) != null){
+                        Controller.dispatchEvent(new StringToFixMessageEvent(this, lineFromLog, nameAlgo, typeMarket));
+                    }
+                }
+            };
+
+
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(timerTask, 0, 1000);
+
+
+
 
         }catch (Exception e) {
             e.printStackTrace();
